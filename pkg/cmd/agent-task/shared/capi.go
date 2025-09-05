5f6189be092cd9dc8e4440e706d826326935e66e
Babak K. Shandiz <babakks@github.com>
2025-09-05 16:58:18 +0100

    fix(agent-task/shared): add `IsSessionID`
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/cmd/agent-task/shared/capi.go
+++ pkg/cmd/agent-task/shared/capi.go
@@ -1,9 +1,13 @@
 package shared
 
 import (
+	"regexp"
+
 	"github.com/cli/cli/v2/pkg/cmd/agent-task/capi"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 )
+
+var uuidRE = regexp.MustCompile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$`)
 
 func CapiClientFunc(f *cmdutil.Factory) func() (capi.CapiClient, error) {
 	return func() (capi.CapiClient, error) {
@@ -20,4 +24,8 @@ func CapiClientFunc(f *cmdutil.Factory) func() (capi.CapiClient, error) {
 		authCfg := cfg.Authentication()
 		return capi.NewCAPIClient(httpClient, authCfg), nil
 	}
+}
+
+func IsSessionID(s string) bool {
+	return uuidRE.MatchString(s)
 }
